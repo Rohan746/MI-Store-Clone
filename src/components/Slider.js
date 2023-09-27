979359ecec3dd52ef6774b01ade2a5ac5adf2e13
Rohan746 <sainirohan2002@gmail.com>
2023-09-27 12:29:45 +0530

    inital commit

---
+++ src/components/Slider.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Slider = ({ start }) => {
+  return (
+    <Carousel fade>
+      {start.map((item, index) => (
+        <Carousel.Item key={index}> {/* Add a unique key */}
+          <img className="d-block w-100" src={item} alt={`Slide ${index + 1}`} />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default Slider;
